39550d7ed2821c276c31a6b71c64053b5ffb9254
atari-monk <atari.monk1@gmail.com>
2024-02-08 10:00:45 +0100

    player moves

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Hello, world!");
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
+        }
+
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate() 
+    {
+        if (dirX > 0f) {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        } 
+        else if (dirX < 0f) 
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        } 
+        else 
+        {
+            anim.SetBool("running", false);
+        }
     }
 }
